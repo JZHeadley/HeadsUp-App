122d1dd5cd448af217d0f4b60e4ed9fa38ea7175
jzheadley <headleyjz@vcu.edu>
2019-03-24 00:59:29 -0400

    have a working bluetooth connection without any issues... what is this... progress?

--- app/src/main/java/com/jzheadley/headsup/MainActivity.kt
+++ app/src/main/java/com/jzheadley/headsup/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.jzheadley.headsup
 
 import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
 import android.location.Location
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import io.nlopez.smartlocation.SmartLocation
+import io.nlopez.smartlocation.location.config.LocationAccuracy
 import io.nlopez.smartlocation.location.config.LocationParams
 import io.nlopez.smartlocation.location.providers.LocationGooglePlayServicesProvider
 import kotlinx.android.synthetic.main.activity_main.*
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothSocket
-import android.view.View
-import java.io.IOException
 import java.io.InputStream
+import java.io.OutputStream
 import java.util.*
 import kotlin.math.abs
 
@@ -22,58 +22,49 @@ import kotlin.math.abs
 const val logt = "MainActivity"
 
 
-val delimiter: Byte = 33
-var readBufferPosition = 0
-
-var mmSocket: BluetoothSocket? = null
-var mmDevice: BluetoothDevice? = null
-
 class MainActivity : AppCompatActivity() {
 
-    internal inner class workerThread(private val btMsg: String) : Runnable {
+    var mmSocket: BluetoothSocket? = null
+    var mmDevice: BluetoothDevice? = null
+    var mBluetoothAdapter: BluetoothAdapter? = null
 
-        override fun run() {
-            sendBtMsg(btMsg)
-        }
-    };
+    val provider = LocationGooglePlayServicesProvider()
+    var params: LocationParams =
+        LocationParams.Builder().setDistance(0f).setInterval(100L).setAccuracy(LocationAccuracy.HIGH)
+            .build()
 
+    val MY_UUID = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee") //Standard SerialPortService ID
+    private var outputStream: OutputStream? = null
+    private var inStream: InputStream? = null
 
     var oldLocation: Location? = null
 
-    @SuppressLint("SetTextI18n")
-    fun locationUpdate(location: Location) {
-        val speedVal = oldLocation?.let { getSpeed(it, location) }
-        val bearingVal = oldLocation?.bearingTo(location)
-        Log.d(
-            logt,
-            "bearing is " + location.hasBearing() + " " + location.bearing + " Altitude is " + location.hasAltitude() + " " + location.altitude + " Latitude is " + location.latitude + " Longitude is " + location.longitude + " speed is " + location.hasSpeed() + " " + speedVal
-        )
-        oldLocation = location
-        speed.text = "Speed ${speedVal.toString()}"
-        bearing.text = "Bearing ${bearingVal.toString()}"
-        if (speedVal != null && bearingVal != null) {
-            Thread(
-                workerThread(
-                    (
-                            "{" +
-                                    "\"lat\":" + location.latitude + "," +
-                                    "\"long\":" + location.longitude + "," +
-                                    "\"speed\":" + abs(speedVal) + "," +
-                                    "\"bearing\":" + bearingVal + "" +
-                                    "}")
-                )
-            ).start()
-        }
-    }
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val provider = LocationGooglePlayServicesProvider()
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        mBluetoothAdapter?.cancelDiscovery()
+        mmDevice = mBluetoothAdapter?.getRemoteDevice("00:15:83:D2:50:D7")
+        mmSocket = mmDevice?.createRfcommSocketToServiceRecord(MY_UUID);
+        mmSocket?.connect()
+
+        outputStream = mmSocket?.outputStream;
+        inStream = mmSocket?.inputStream;
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mmSocket?.close()
+    }
+    override fun onStart() {
+        super.onStart()
         SmartLocation.with(this)
             .location(provider)
-            .config(LocationParams.NAVIGATION)
+            .config(params)
             .continuous()
             .start {
                 run {
@@ -81,50 +72,11 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        val pairedDevices = mBluetoothAdapter.bondedDevices
-        if (pairedDevices.size > 0) {
-            for (device in pairedDevices) {
-                Log.d(logt, device.name)
-                if (device.name == "raspberrypi")
-                //Note, you will need to change this to match the name of your device
-                {
-                    Log.e(logt, device.name)
-                    mmDevice = device
-                    break
-                }
-            }
-        }
-        startBluetooth.setOnClickListener {
-            Thread(workerThread("blah")).start()
-        }
-
-    }
-
-    fun sendBtMsg(msg2send: String) {
-//        val uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
-        val uuid = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee") //Standard SerialPortService ID
-        try {
-            Log.d(logt, "Sending bluetooth message")
-            mmSocket = mmDevice?.createRfcommSocketToServiceRecord(uuid)
-            if (!mmSocket?.isConnected!!) {
-                mmSocket!!.connect()
-            }
-
-            val mmOutputStream = mmSocket!!.getOutputStream()
-            mmOutputStream.write(msg2send.toByteArray())
-
-        } catch (e: IOException) {
-            // TODO Auto-generated catch block
-            e.printStackTrace()
-        }
-
     }
 
     fun getSpeed(currentLocation: Location, oldLocation: Location): Double {
         val newLat = currentLocation.latitude
         val newLon = currentLocation.longitude
-
         val oldLat = oldLocation.latitude
         val oldLon = oldLocation.longitude
 
@@ -144,8 +96,29 @@ class MainActivity : AppCompatActivity() {
             val timeDifferent = (currentLocation.time - oldLocation.time).toDouble()
             return distance / timeDifferent * 1000
         }
+    }
 
 
+    @SuppressLint("SetTextI18n")
+    fun locationUpdate(location: Location) {
+        val speedVal = oldLocation?.let { getSpeed(it, location) }
+        val bearingVal = oldLocation?.bearingTo(location)
+        Log.d(
+            logt,
+            "bearing is " + location.hasBearing() + " " + location.bearing + " Altitude is " + location.hasAltitude() + " " + location.altitude + " Latitude is " + location.latitude + " Longitude is " + location.longitude + " speed is " + location.hasSpeed() + " " + speedVal
+        )
+        oldLocation = location
+        speed.text = "Speed ${speedVal.toString()}"
+        bearing.text = "Bearing ${bearingVal.toString()}"
+        if (speedVal != null && bearingVal != null) {
+            var message =
+                "{\"lat\":" + location.latitude + "," +
+                        "\"long\":" + location.longitude + "," +
+                        "\"speed\":" + abs(speedVal) + "," +
+                        "\"bearing\":" + bearingVal + "" +
+                        "}"
+            outputStream?.write(message.toByteArray())
+        }
     }
 
 }
